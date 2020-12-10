aad65142d2a7c6767777dc09acfc11e87e28a52f
ATabdessamia <abdessamia.aitlahcen@gmail.com>
2020-12-11 00:46:14 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
