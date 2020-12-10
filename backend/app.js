fabc9166e479eee665f52e72196f74b681ef964b
ATabdessamia <abdessamia.aitlahcen@gmail.com>
2020-12-10 23:15:07 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -15,14 +15,13 @@ app.use(express.json());
 
 app.use("/api/tasks", taskRoutes);
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(path.resolve(), "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => {
