aa5d4774b1f1450abed81b0327012f42015b626c
ATabdessamia <abdessamia.aitlahcen@gmail.com>
2020-12-11 00:42:25 +0100

    .

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import path from "path";
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
 
 import taskRoutes from "./routes/taskRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
 
 const app = express();
 
@@ -18,9 +21,7 @@ app.use("/api/tasks", taskRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
-  app.get(
-    "*",
-    (req, res) => res.setHeader("Content-Type", "text/plain"),
+  app.get("*", (req, res) =>
     res.sendFile(
       path.resolve(path.resolve(), "frontend", "build", "index.html")
     )
