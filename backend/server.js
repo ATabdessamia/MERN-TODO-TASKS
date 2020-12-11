22dfaf0b5b5948d8af759abeeda490a6fb1af559
ATabdessamia <abdessamia.aitlahcen@gmail.com>
2020-12-11 01:27:09 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,48 @@
 import color from "colors";
 import dotenv from "dotenv";
+import path from "path";
+import express from "express";
+import morgan from "morgan";
 
 import connectDb from "./config/db.js";
+import taskRoutes from "./routes/taskRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
-
-import app from "./app.js";
-
 connectDb();
 
-const PORT = process.env.PORT || 8000;
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.use("/api/tasks", taskRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(path.resolve(), "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 8000;
 
 app.listen(
-  PORT,
+  port,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold
   )
 );
